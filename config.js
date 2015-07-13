0c066ae35798421da3123eadeb30543a04915143
muit <miguel_3c@hotmail.com>
2015-07-13 11:19:07 +0200

    Implemented https static server

--- config.js
+++ config.js
@@ -1,13 +1,34 @@
 module.exports = {
   development: {
-    manifests: ["localhost:3000/manifest.json"]
+    manifests: ["localhost:3000/manifest.json"],
+
+    serveme: {
+      secure: true,
+      key: "./ssl/server-key.pem",
+      cert: "./ssl/server-cert.pem",
+    }
   },
 
   test: {
-    manifests: ["localhost:3000/manifest.json"]
+    manifests: ["localhost:3000/manifest.json"],
+
+    serveme: {
+      log: false,
+      secure: true,
+      key: "./ssl/server-key.pem",
+      cert: "./ssl/server-cert.pem",
+    }
   },
 
   production: {
-    manifests: []
+    manifests: ["localhost:3000/manifest.json"],
+
+    serveme: {
+      log: false,
+      cache: true,
+      secure: true,
+      key: "./ssl/server-key.pem",
+      cert: "./ssl/server-cert.pem",
+    }
   }
 };
