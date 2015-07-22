b8aa995293330bb6135814eab1c012eb27c6a7f0
muit <miguel_3c@hotmail.com>
2015-07-22 17:28:42 +0200

    New command: "-g config"

---
+++ docs/configExample.js
@@ -0,0 +1,30 @@
+module.exports = {
+  default: {
+    manifests: [],
+
+    port: 3000,
+
+    secure: true,
+    
+    /** Https
+    express: {
+      key: "./ssl/server-key.pem",
+      cert: "./ssl/server-cert.pem"
+    },
+    **/
+
+    server: {
+      index: "app/views/index.html"
+    }
+  },
+
+  development: {},
+
+  test: {
+    secure: false
+  },
+
+  production: {
+    port: 80,
+  }
+};
--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-Core = require('./lib/core.js');
+var fs = require('fs-extra');
+
+var Core = require('./lib/core.js');
 
 var args = process.argv.slice(2);
 
 var port;
 var envelopment = "development";
 
-for (var i = 0; i < args.length; i++) {
-  if (args[i] == "--port" || args[i] == "-p") {
-    //port command
-    i++;
-    port = args[i];
-  } else if (args[i] == "--env" || args[i] == "-e") {
-    //environment command
-    i++;
-    envelopment = args[i];
+function start() {
+  for (var i = 0; i < args.length; i++) {
+    if (args[i] === "--port" || args[i] === "-p") {
+      //port command
+      i++;
+      port = args[i];
+    } else if (args[i] === "--env" || args[i] === "-e") {
+      //environment command
+      i++;
+      envelopment = args[i];
+    } else if (args[i] === "--generate" || args[i] === "-g") {
+      i++;
+      if (args[i] === "config") {
+        generateConfig();
+        return;
+      }
+      console.log("Invalid command:\n  COMMAND -g config");
+      process.exit(0);
+    }
   }
+  Core(envelopment, port);
 }
 
-Core(envelopment, port);
+function generateConfig() {
+  console.log("Generating config...");
+  fs.copy('docs/configExample.js', 'config.js', {
+    clobber: true
+  }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Done!");
+    }
+    process.exit(0);
+  });
+}
+
+start();
