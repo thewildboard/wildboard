4bd66b0b228e690fb3d6b34ced3e857bdd276621
guardabrazo <federico.guardabrazo@gmail.com>
2015-07-31 13:25:48 +0200

    Test

--- public/app/controllers/panelController.js
+++ public/app/controllers/panelController.js
@@ -4,20 +4,27 @@ controller('panelController', function ($scope) {
   var controller = $scope.gridster;
   controller.tab = 1;
   controller.image_selected = 1;
+  controller.colorSelected = 0;
+
   controller.selectTab = function(setTab){
     controller.tab = setTab;
-  }
+  };
 
   controller.isSelected = function(checkTab){
     return controller.tab === checkTab;
-  }
+  };
 
   controller.isImageSelected = function(image){
-    return controller.image_selected === image
+    return controller.image_selected === image;
   };
 
   controller.selectImage = function(image){
     controller.image_selected = image;
+  };
+
+  controller.selectColor = function(color){
+    controller.colorSelected = color;
+    console.log(controller.colorSelected);
   };
 
 });
